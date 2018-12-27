3c7f574e773b77e52199a80a5d0059928e21c045
kovner chalumo <kchalumo@gmail.com>
2018-12-27 06:10:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(sel){
+  return document.querySelector(sel);
+}
 
+function nestedTarget() {
+  var nest = document.querySelector("#nested");
+  return nest.querySelector(".target");
+}
+function deepestChild(){
+  //not sure if this is an appropriate solution I don't
+  //think i understand the problem well enough 
+  var grandDiv = document.querySelector("#grand-node");
+  var allDivs = grandDiv.querySelectorAll("div");
+  return allDivs[allDivs.length - 1];
+}
+function increaseRankBy(n) {
+  const lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
