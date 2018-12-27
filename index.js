3bc5fde3ab949e9b3d86b4e48980fc0d05311071
kovner chalumo <kchalumo@gmail.com>
2018-12-27 05:48:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function getFirstSelector(sel){
+  return document.querySelector(sel);
+}
 
+function nestedTarget() {
+  var nest = document.getElementById("#nested");
+  return nest.querySelector("div");
+}
